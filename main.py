18f8847b473d2b466fed3583dc0c7e3d31a8aa19
darkroom2 <rkomorowski97@gmail.com>
2021-02-28 21:28:45 +0100

    test script

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sched
+import threading
 from pathlib import Path
+from time import sleep
 
 import devicemonitorlib.devicemonitorfunctions as device_monitor
 
@@ -59,6 +61,61 @@ class DeviceByFile:
 #             return str(response)
 
 
+def main():
+    # Lista urzadzen do monitorowania.
+    device_list = [
+        DeviceByFile('1', ['current', 'voltage', 'pressure'], './devices/zasilacz_00001.json'),
+        DeviceByFile('2', ['current', 'voltage'], './devices/zasilacz_00002.json'),
+        DeviceByFile('3', ['current', 'voltage'], './devices/zasilacz_00003.json'),
+        # DeviceByHTTP('4', ['current', 'voltage', 'pressure'], '127.0.0.1', 6666),
+    ]
+
+    # Jak czesto odswiezac parametry urzadzenia
+    update_interval = 1
+
+    print('Main thread is starting the DeviceMonitor...')
+    device_monitor.start(update_interval, device_list)
+    print('DeviceMonitor started successfully!')
+
+    sleep(1)
+
+    # Scheduler wypisujacy cyklicznie parametry
+    s = sched.scheduler()
+
+    # Jak czesto wypisywac statusy urzadzen
+    print_interval = 1
+
+    print('Main thread is creating a new thread with scheduler that periodically calls the get_statuses() method...')
+    t = threading.Thread(target=start_print_scheduler, args=(s, print_interval))
+    t.start()
+    print('Thread with print scheduler started successfully!')
+
+    sleep(1)
+
+    print('Main thread sleeping for 5 secs while other thread prints...')
+    sleep(5)
+
+    print('Main thread calling get_statuses()...')
+    print(device_monitor.get_statuses())
+    print('Main thread called get_statuses() successfully!')
+
+    sleep(5)
+
+    print('Main thread stops the DeviceMonitor...')
+    device_monitor.stop()
+    print('Main thread stopped the DeviceMoniotor successfully!')
+
+    sleep(5)
+
+    print('Main thread stops the printer thread...')
+    stop_scheduler(s)
+    print('Printer thread stopped successfully!')
+
+    sleep(5)
+
+    print('Main thread is ending...')
+
+
 def print_statuses(s, print_interval, statuses):
     """Funkcja wypisujaca statusy urzadzen
 
@@ -79,32 +136,32 @@ def print_statuses(s, print_interval, statuses):
     print(statuses)
 
 
-def main():
-    # Lista urzadzen do monitorowania.
-    device_list = [
-        DeviceByFile('1', ['current', 'voltage', 'pressure'], './devices/zasilacz_00001.json'),
-        DeviceByFile('2', ['current', 'voltage'], './devices/zasilacz_00002.json'),
-        DeviceByFile('3', ['current', 'voltage'], './devices/zasilacz_00003.json'),
-        # DeviceByHTTP('4', ['current', 'voltage', 'pressure'], '127.0.0.1', 6666),
-    ]
+def start_print_scheduler(s, print_interval):
+    """Funkcja uruchamiajaca cykliczne wypisywanie parametrow
 
-    # Jak czesto odswiezac parametry urzadzenia
-    params_update_interval = 1
-
-    # Uruchomienie monitorowania
-    device_monitor.start(params_update_interval, device_list)
-
-    # Scheduler wypisujacy cyklicznie parametry
-    s = sched.scheduler()
-
-    # Jak czesto pobierac statusy urzadzen
-    print_interval = 1
-
+    Parameters
+    ----------
+    s
+        Referencja schedulera
+    print_interval
+        Czas odstepu miedzy kolejnymi wypisaniami parametrow
+    """
     # Dodanie pierwszego wywolania funkcji wypisujacej do schedulera z zerowym oczekiwaniem
     s.enter(0, 0, print_statuses, (s, print_interval, device_monitor.get_statuses()))
-
     # Uruchomienie schedulera
     s.run()
+
+
+def stop_scheduler(s):
+    """Funkcja czyszczaca scheduler
+
+    Parameters
+    ----------
+    s
+        Referencja schedulera do wyczyszczenia
+    """
+    for ev in s.queue:
+        s.cancel(ev)
 
 
 if __name__ == '__main__':
