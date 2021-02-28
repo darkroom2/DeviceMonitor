e8a7e1d8cdf248a2f79615f3a39e96503bac0da2
darkroom2 <rkomorowski97@gmail.com>
2021-02-28 21:28:38 +0100

    ver update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='devicemonitorlib',
     packages=find_packages(include=['devicemonitorlib']),
-    version='0.1.0',
+    version='0.1.1',
     description='A library for monitoring various types of devices.',
     author='Radoslaw Komorowski',
     license='MIT',
