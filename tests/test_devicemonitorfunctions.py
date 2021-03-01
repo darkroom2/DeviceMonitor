7cf453b4881b0398f91ba0b255310430f84c2322
darkroom2 <rkomorowski97@gmail.com>
2021-03-01 02:52:47 +0100

    pedantic update

--- tests/test_devicemonitorfunctions.py
+++ tests/test_devicemonitorfunctions.py
@@ -33,34 +33,46 @@ true_dict = {
 
 @pytest.mark.parametrize(('interval', 'devices_list'), [(0, []), (1, []), (1, device_list)])
 def test_stop(interval, devices_list):
+    # Wyczyszczenie biblioteki z ewentualnych poprzednich wywolan
     dm.stop()
     sleep(1)
 
+    # Uruchomienie biblioteki
     dm.start(interval, devices_list)
     sleep(1)
 
+    # Zatrzymanie biblioteki
     dm.stop()
     sleep(3)
-    assert len(threading.enumerate()) == 1 and dm.running is False and dm.statuses == {} and not dm.s.queue
+
+    # Sprawdzenie czy watek zostal zamkniety i czy zostaly 'wyzerowane' zmienne oraz czy kolejka schedulera jest pusta
+    assert (len(threading.enumerate()) == 1)\
+           and (dm.running is False) \
+           and dm.statuses == {} \
+           and not dm.s.queue, 'Thread wasnt properly closed!'
 
 
 @pytest.mark.parametrize(('interval', 'devices_list'), [(0, []), (1, []), (1, device_list)])
 def test_start(interval, devices_list):
     errors = []
 
+    # Wyczyszczenie biblioteki z ewentualnych poprzednich wywolan
     dm.stop()
     sleep(1)
 
+    # Uruchomienie biblioteki
     dm.start(interval, devices_list)
     sleep(1)
 
-    # Jesli poza watkiem glownym nie ma innego watku to blad
+    # Jesli watek biblioteki nie zostal uruchomiony to dodajemy blad do listy
     if len(threading.enumerate()) == 1:
         errors.append(f'Thread with params: {interval}, {devices_list} is not running.')
 
+    # Jesli zmienna running nie zostala ustawiona na True, to dodajemy blad do listy
     if not dm.running:
         errors.append('Function did not finish!')
 
+    # Sprawdzenie czy lista bledow jest pusta, jesli nie, to wypisanie komunikatow bledow
     assert not errors, 'Errors occured:\n{}'.format('\n'.join(errors))
 
 
@@ -68,61 +80,75 @@ def test_start(interval, devices_list):
                                                   (device_list[1], true_dict[device_list[1].id]),
                                                   (device_list[2], true_dict[device_list[2].id])])
 def test_update_status(device, expected):
+    # Wyczyszczenie biblioteki z ewentualnych poprzednich wywolan
     dm.stop()
     sleep(1)
 
+    # Pobranie aktualnych wartosci parametrow konkretnego urzadzenia
     dm.update_status(device)
-    assert dm.statuses[device.id] == expected
+
+    # Sprawdzenie czy funkcja utworzyla odpowiedni status danego urzadzenia
+    assert dm.statuses[device.id] == expected, 'Dicts doesnt match!'
 
 
-@pytest.mark.parametrize(('interval', 'devices_list', 'expected'), [(0, [], {}), (1, [], {}), (1, device_list, true_dict)])
+@pytest.mark.parametrize(('interval', 'devices_list', 'expected'),
+                         [(0, [], {}), (1, [], {}), (1, device_list, true_dict)])
 def test_update_statuses(interval, devices_list, expected):
+    # Wyczyszczenie biblioteki z ewentualnych poprzednich wywolan
     dm.stop()
     sleep(1)
 
+    # Pobranie aktualnych wartosci parametrow wszystkich urzadzen
     dm.update_statuses(interval, devices_list)
-    assert dm.s.queue and dm.statuses == expected
+
+    # Sprawdzenie czy utworzono odpowiedni status urzadzen oraz czy zainicjowano scheduler
+    assert dm.s.queue and dm.statuses == expected, 'Dicts doesnt match!'
 
 
 @pytest.mark.parametrize(('interval', 'devices_list'), [(0, []), (1, []), (1, device_list)])
 def test_start_monitoring(interval, devices_list):
     errors = []
 
+    # Wyczyszczenie biblioteki z ewentualnych poprzednich wywolan
     dm.stop()
     sleep(1)
 
+    # Utworzenie watku ze schedulerem pobierajacym cyklicznie parametry urzadzen
     t = threading.Thread(target=dm.start_monitoring, args=(interval, devices_list))
     t.start()
-
     sleep(1)
 
+    # Sprawdzenie czy udalo sie uruchomi scheduler, jesli jego kolejka jest pusta, to dodajemy blad
     if not dm.s.queue:
         errors.append('Schedulers queue empty!')
 
+    # Zatrzymanie biblioteki
     dm.stop()
     sleep(1)
 
+    # Sprawdzenie czy lista bledow jest pusta, jesli nie, to wypisanie komunikatow bledow
     assert not errors, 'Errors occured:\n{}'.format('\n'.join(errors))
 
 
 @pytest.mark.parametrize(('interval', 'devices_list', 'expected'),
                          [(0, [], {}), (1, [], {}), (1, device_list, true_dict)])
 def test_get_statuses(interval, devices_list, expected):
-    """Symuluje odswiezanie parametrow
-
-    """
+    # Wyczyszczenie biblioteki z ewentualnych poprzednich wywolan
     dm.stop()
     sleep(1)
 
+    # Uruchomienie biblioteki
     dm.start(interval, devices_list)
     sleep(1)
-    assert expected == dm.get_statuses(), 'Dicts doesnt match!'
+
+    # Sprawdzenie zgodnosci slownikow
+    assert dm.get_statuses() == expected, 'Dicts doesnt match!'
 
 
 def test_get_statuses_without_start():
-    """Symuluje pobranie statusow bez wystartowania biblioteki
-
-    """
+    # Wyczyszczenie biblioteki z ewentualnych poprzednich wywolan
     dm.stop()
     sleep(1)
-    assert dm.get_statuses() is None, 'Should be None if DeviceMonitor isnt running.'
+
+    # Jesli nie uruchomiono biblioteki, testowana funkcja powinna zwracac None
+    assert dm.get_statuses() is None, 'Should be None if DeviceMonitor isnt running!'
